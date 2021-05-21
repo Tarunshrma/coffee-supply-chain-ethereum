edf73440009a717644f623bcc3a7ba11b3b503e9
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2021-05-21 20:33:54 +0530

    Added depoyment configuration

--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "64cc5f0a9bf647339ba58bb90f3411bb";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 9545,
       network_id: "*" // Match any network id
-    }
+      //websockets: true
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    },
   }
 };
